d7336070defefc9dc4fe9241ad29dbd45546560d
asma828 <asmalachhab66@gmail.com>
2025-12-17 08:58:29 +0100

    initialiser le project

---
+++ src/main/java/com/example/Al/Baraka/AlBarakaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Al.Baraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlBarakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlBarakaApplication.class, args);
+	}
+
+}
